e03bc79cf12f45f3345f9bc4c30aa8a39f1a293c
tatatititutu-a <arinapramudta@gmail.com>
2024-10-06 02:16:36 +0700

    app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -92,8 +92,3 @@ if __name__ == "__main__":
 
     # menampilkan visualisasi
     st.title('Bike Rental Data Visualization')
-
-    explore_data(df)
-    analyze_holiday_impact(df)
-    analyze_weather_impact(df)
-    clustering_analysis(df)
